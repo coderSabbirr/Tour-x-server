5e4a81b748a2c629e9cd9b81091fa862d5c2eb90
Nahid Hasan Sabbir <sabbirnahid5817@gmail.com>
2021-10-30 01:37:34 +0600

    main add

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express=require('express');
+const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config()
-const cors=require('cors')
+const cors = require('cors')
 const ObjectId = require('mongodb').ObjectId
-const app=express();
-const port= process.env.PORT || 4000;
+const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
@@ -19,26 +19,66 @@ async function run() {
         const packageCollection = database.collection('package');
         const orderCollection = database.collection('orders');
 
-     
-       
+
+
 
         app.get('/packages', async (req, res) => {
-            const cursor=packageCollection.find({})
-            const package= await cursor.toArray();
+            const cursor = packageCollection.find({})
+            const package = await cursor.toArray();
             res.send(package)
-        
-        });
-        app.get('/packages/:id', async(req,res) => {
-            const id =req.params.id;
-            const query={_id: ObjectId(id)};
-            const service= await packageCollection.findOne(query);
-            res.json(service);
-        })
-        
 
+        });
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({})
+            const orders = await cursor.toArray();
+            res.send(orders)
+
+        });
+        app.get('/packages/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const package = await packageCollection.findOne(query);
+            res.json(package);
+        })
+
+        app.get("/orders/:email", async (req, res) => {
+            const result = await orderCollection.find({
+                email: req.params.email,
+            }).toArray();
+            res.send(result);
+        });
+        app.get("/addnew/:newpackage", async (req, res) => {
+            const result = await packageCollection.find({
+                newpackage: req.params.newpackage,
+            }).toArray();
+            res.send(result);
+        });
+
+
+        app.post("/packages", async (req, res) => {
+            const result = await orderCollection.insertOne(req.body);
+
+        });
+        app.post("/addpackages", async (req, res) => {
+            const result = await packageCollection.insertOne(req.body);
+            res.send(result);
+          });
+
+        app.delete('/orders/:id', async (req, res) => {
+            const query = { _id: ObjectId(req.params.id) }
+            const result = await orderCollection.deleteOne(query)
+            res.json(result);
+
+        })
+        app.delete('/packages/:id', async (req, res) => {
+            const query = { _id: ObjectId(req.params.id) }
+            const result = await packageCollection.deleteOne(query)
+            res.json(result);
+
+        })
     }
     finally {
-        // await client.close();
+
     }
 }
 
